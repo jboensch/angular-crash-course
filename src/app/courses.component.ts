0ad14771e1eb8c45490b05828ebee2c053043fcb
Joshua Boensch <josh.boensch@gmail.com>
2018-10-19 14:43:23 -0400

    Section 3 - Lecture 33

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,11 +3,16 @@ import { componentFactoryName } from '@angular/compiler';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesCompoment {
     title = "List of courses";
-    getTitle() {
-        return this.title;
-    }
+    courses = ["Course 1", "Course 2", "Course 3"];
 }
