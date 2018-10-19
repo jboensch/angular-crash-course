aa7f858dc35bc04b0f58404700ec2c58e8f9ffca
Joshua Boensch <josh.boensch@gmail.com>
2018-10-19 14:34:00 -0400

    Section 3 - Lecture 31

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoursesCompoment } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesCompoment
+    CoursesCompoment,
+    CourseComponent
   ],
   imports: [
     BrowserModule
