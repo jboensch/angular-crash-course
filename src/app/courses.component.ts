ae85e67f9fb502d323cc2d90307c867c04167955
Joshua Boensch <josh.boensch@gmail.com>
2018-10-19 14:12:24 -0400

    Section 3 - Lesson 30

---
+++ src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import {Component } from '@angular/core';
+import { componentFactoryName } from '@angular/compiler';
+
+@Component({
+    selector: 'courses',
+    template: '<h2>Course</h2>'
+})
+export class CoursesCompoment {
+
+}
